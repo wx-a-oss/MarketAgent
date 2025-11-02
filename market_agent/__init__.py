99d9b5aef3a0bca318a60bc1e705e3ec70fed372
wx-a-oss <awx3140100905@gmail.com>
2025-11-02 15:34:34 -0800

    add framework for modules, no content inside

--- market_agent/__init__.py
+++ market_agent/__init__.py
@@ -1,2 +1,4 @@
-from .agent import MarketAgent
-__all__ = ["MarketAgent"]
+from market_agent.agent import MarketAgent
+from market_agent.api import query_stock
+
+__all__ = ["MarketAgent", "query_stock"]
